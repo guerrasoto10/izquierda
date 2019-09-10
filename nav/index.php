907022e09edc2994dc3610a38332de0fa356e63e
guerrasoto10 <gaguerra84@gmail.com>
2019-09-09 21:28:29 -0400

    contando los ariculos en descripcion

--- nav/index.php
+++ nav/index.php
@@ -1,7 +1,7 @@
 <?php
 include "database.php";
 
-$datos = $con->query("SELECT Factura, Cliente, Fecha, RIF_Cliente, Cod_Cliente, GROUP_CONCAT(Descripcion SEPARATOR '<br>') AS Descripcion, sum(Base) as Base, sum(Importe) as Importe, sum(Costo) as Costo, COUNT(Factura) as Numero 
+$datos = $con->query("SELECT Factura, Cliente, Fecha, RIF_Cliente, Cod_Cliente, COUNT(Descripcion) AS Descripcion, sum(Base) as Base, sum(Importe) as Importe, sum(Costo) as Costo, COUNT(Factura) as Numero 
 FROM person  
 where Factura<>'' and Fecha<>''
 GROUP BY Factura, Cliente, Fecha, RIF_Cliente, Cod_Cliente");
@@ -59,39 +59,47 @@ GROUP BY Factura, Cliente, Fecha, RIF_Cliente, Cod_Cliente");
     </ul>
 
 
+    <?php if($datos->num_rows>0):?>
+	<h3>Datos</h3>
+    <p>Facturas Encontradas: <?php echo $datos->num_rows; ?></p>
+    
     <table class="responsive-table">
       <thead>
         <tr>
-            <th>Izq</th>
-            <th>Nombre</th>
-            <th>Producto</th>
-            <th>Precio</th>
+    <th>Izq</th>
+		<th>Factura</th>
+		<th>Fecha</th>
+		<th>Cliente</th>
+		<th>RIF Cliente</th>
+		<th>Cod. Cliente</th>
+		<th>Cant. Prod</th>
+		<th>Base</th>
+		<th>Importe</th>
+		<th>Costo</th>
         </tr>
       </thead>
       
       <tbody>
-        <tr>
-          <td><label><input type="checkbox"/><span></span></label></td>
-          <td>Gustavo</td>
-          <td class="btn-flat tooltipped" data-position="right" data-tooltip="I am a tooltip">Pintura</td>
-          <td>100</td>
-        </tr>
-
-        <tr>
-          <td><label><input type="checkbox"/><span></span></label></td>
-          <td>Pedro</td>
-          <td>Cemento</td>
-          <td>200</td>
-        </tr>
-        <tr>
-          <td><label><input type="checkbox"/><span></span></label></td>
-          <td>Juan</td>
-          <td>Llave</td>
-          <td>300</td>
-        </tr>
+      <?php while($d= $datos->fetch_object()):?>
+      <tr>
+      <td><label><input type="checkbox"/><span></span></label></td>
+		<td><?php echo $d->Factura; ?></td>
+		<td><?php echo $d->Fecha; ?></td>
+		<td><?php echo $d->Cliente; ?></td>
+		<td><?php echo $d->RIF_Cliente; ?></td>
+		<td><?php echo $d->Cod_Cliente; ?></td>
+		<td style="margin-top: 13px" class="btn tooltipped" data-position="right" data-tooltip="I am a tooltip"><?php echo $d->Descripcion; ?></td>
+    <td><?php echo $d->Base; ?></td>
+		<td><?php echo $d->Importe; ?></td>
+		<td><?php echo $d->Costo; ?></td>
+		</tr>
+        <?php endwhile; ?>
       </tbody>
     </table>
-</br>
+    <?php else:?>
+	<h3>No hay Datos</h3>
+<?php endif; ?>
+
 <!-- div para alinear el boton de Generar -->
 <div style="text-align: right">
   <button class="btn waves-effect waves-light" type="submit" name="action">Generar Izq
